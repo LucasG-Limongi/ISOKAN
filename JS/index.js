c5fd915f01a0f6d08bc9c782f1caecace21afcf4
LucasG-Limongi <gabriel.limongi@aluno.ifsp.edu.br>
2025-10-11 22:20:32 -0300

    our-mission começado e nav-mobile alterado

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,10 @@
 $('.menu-mobile i').click(function () {
     $('.nav-mobile').slideToggle();
 })
+
+$(window).resize(function() {
+    if ($(window).width < 900) {
+        $('.nav-mobile, .menu-mobile').css('display', 'none')
+        $('.nav').css('display', 'flex')
+    }
+}) // tentativa de corrigir o bug do menu mobile que aparece no desktop
