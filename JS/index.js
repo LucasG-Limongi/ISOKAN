e65f5232903840c93fe9b69113698f5681e20cb5
LucasG-Limongi <gabriel.limongi@aluno.ifsp.edu.br>
2025-10-13 22:41:57 -0300

    commit 13/10 22:41 começo da galeria no index e do footer e avanço no index.js com passador de slide
    Co-authored-by: Vivawig <Vivawig@users.noreply.github.com>

--- JS/index.js
+++ JS/index.js
@@ -8,3 +8,42 @@ $(window).resize(function() {
         $('.nav').css('display', 'flex')
     }
 }) // tentativa de corrigir o bug do menu mobile que aparece no desktop
+
+// Carrosel do index 
+
+const slider = document.querySelectorAll('.slider')
+const btnPrev = document.getElementById('voltar')
+const btnNext = document.getElementById('avancar')
+
+let slideAtivo = 0
+
+function esconderSlide() {
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function mostrarSlide() {
+    slider[slideAtivo].classList.add('on')
+}
+
+function nextSlide() {
+    esconderSlide()
+    if(slideAtivo == slider.length - 1) {
+        slideAtivo = 0
+    } else {
+        slideAtivo++
+    }
+    mostrarSlide()
+}
+
+function prevSlide() {
+    esconderSlide()
+    if(slideAtivo == 0) {
+        slideAtivo = slider.length -1
+    } else {
+        slideAtivo--
+    }
+    mostrarSlide()
+}
+
+btnNext.addEventListener('click', nextSlide)
+btnPrev.addEventListener('click', prevSlide)
